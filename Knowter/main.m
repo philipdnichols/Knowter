493733eb6d928e7ab7401ccd113afb2b213df84d
Phil Nichols <philipdnichols@gmail.com>
2014-08-06 17:34:20 -0400

    initial commit

---
+++ Knowter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Knowter
+//
+//  Created by Philip Nichols on 8/6/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KnowterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KnowterAppDelegate class]));
+    }
+}
